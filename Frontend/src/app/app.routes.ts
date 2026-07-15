f3da6732b1517b2b3dcb9b67d1322f27d1e02bbd
Emirhan <emirhankaracoban@gmail.com>
2026-07-15 19:13:50 +0300

    feat: Routing yapılandırıldı, Dashboard ve Tasks sayfaları eklendi

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Dashboard } from './features/dashboard/dashboard';
+import { Tasks } from './features/tasks/tasks';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: Dashboard },
+  { path: 'tasks', component: Tasks }
+];
